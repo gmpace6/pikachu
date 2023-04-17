e6d5259dfe612853b7c6b2d34a4a2b08604333d8
Garrett Pace <elderpace6@gmail.com>
2023-04-17 14:14:44 -0600

    1stCommit

---
+++ function-drills/walkthrough.js
@@ -0,0 +1,11 @@
+let pikachuHp= 100
+
+// turns pikachuHp from a number into a string
+pikachuHp = String(pikachuHp)
+    
+let shieldonHp = 120
+
+let theWinnerIs = 'The winner is Pikachu'
+
+theWinnerIs.replace('Pikachu', 'Shieldon')
+console.log(theWinnerIs)
